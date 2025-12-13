3a1dc54bcc9069ea5582dd5e7698c49000e93a72
VictorVwV <vioreanuvictor66@gmail.com>
2025-12-13 20:50:13 +0200

    gg

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: " ",
-  images: { unoptimized: true },
-
+  output: "export",              // required for static export
+  basePath: "/osfiirbuy",        // must match your GitHub repo name
+  images: { unoptimized: true }, // needed for Next.js Image on static export
 };
-
 
 export default nextConfig;
