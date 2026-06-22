49266b4823bc0344611caeb371ffe6c14712ad1d
VictorVwV <vioreanuvictor66@gmail.com>
2026-06-22 13:58:08 +0300

    Updates

---
+++ _next/static/n-RBtAX8ZsKhtddflBiSj/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/osfiirbuy//_next/:path+",
+        "destination": "/osfiirbuy/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
